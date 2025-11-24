da7709ec89179254c59c61ef9f2e26f3189beaa2
sTheNight <sthenight@outlook.com>
2025-11-25 01:18:06 +0800

    refactor: 重构 ApiHelper 的单例模式

--- WebApplication1/Utils/ApiHelper.cs
+++ WebApplication1/Utils/ApiHelper.cs
@@ -2,14 +2,18 @@
 
 namespace SFSCtinstallerAPI.Utils {
     internal static class ApiHelper {
-        private static readonly HttpClient HttpClient;
+        private static readonly Lazy<HttpClient> LazyHttpClient =
+            new Lazy<HttpClient>(() => InitializeHttpClient());
 
-        static ApiHelper() {
-            HttpClient = new HttpClient();
-            HttpClient.DefaultRequestHeaders.Add("Accept", "application/vnd.github+json");
-            HttpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {Global.GITHUB_TOKEN}");
-            HttpClient.DefaultRequestHeaders.Add("X-GitHub-Api-Version", "2022-11-28");
-            HttpClient.DefaultRequestHeaders.Add("User-Agent", "Github-REST-API-Test");
+        private static HttpClient HttpClient => LazyHttpClient.Value;
+
+        private static HttpClient InitializeHttpClient() {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/vnd.github+json");
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {Global.GITHUB_TOKEN}");
+            client.DefaultRequestHeaders.Add("X-GitHub-Api-Version", "2022-11-28");
+            client.DefaultRequestHeaders.Add("User-Agent", "Github-REST-API-Test");
+            return client;
         }
 
         public static async Task<JObject> GetArtifacts(int perPage = 1, int page = 1) {
@@ -38,6 +42,7 @@ namespace SFSCtinstallerAPI.Utils {
             var content = await response.Content.ReadAsStringAsync();
             return JObject.Parse(content);
         }
+
         public static async Task<Stream> DownloadArtifact(string artifactId) {
             var artifactInfo = await GetArtifactById(artifactId);
             var url = artifactInfo["archive_download_url"]?.ToString();
